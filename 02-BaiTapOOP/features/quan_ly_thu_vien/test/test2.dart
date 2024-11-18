fbbb653a161444296a3369e291c82b6b5efd4fe2
Trần Mai Trung Nguyên <163755944+liduc@users.noreply.github.com>
2024-11-18 16:02:51 +0700

    Delete 02-BaiTapOOP directory

--- 02-BaiTapOOP/features/quan_ly_thu_vien/test/test2.dart
+++
@@ -1,9 +0,0 @@
-import '../models/sach.dart';
-import '../models/thuvien.dart';
-import '../models/docgia.dart';
-
-void main() {
-  var sach = Sach("001", "Lập trình Dart", "Nguyễn Văn A");
-
-  sach.hienthiThongTin();
-}
